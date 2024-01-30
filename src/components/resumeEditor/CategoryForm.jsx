b42e8687bbd5463123a6ec8a4213c3dd5a8aa43f
e-lamine <lamine@e-lamine.dev>
2024-01-30 17:21:33 +0100

    resumeEditor form using MUI components

--- src/components/resumeEditor/CategoryForm.jsx
+++ src/components/resumeEditor/CategoryForm.jsx
@@ -1,11 +1,14 @@
-import { createElement } from 'react';
+import { createElement, useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
 
 function CategoryForm({ activeTab, selectedResume, config, saveData }) {
    const [formData, setFormData] = useState([]);
-   
-   const handleSubmit = (e) => {
 
-   }
+   const handleSubmit = (e) => {};
 
    return (
       <div>
@@ -21,16 +24,39 @@ function CategoryForm({ activeTab, selectedResume, config, saveData }) {
             })}
          </ul>
          <form action="">
-            {config[activeTab].map((input, index) => {
+            {config[activeTab].map((field, index) => {
                return (
                   <div key={index}>
-                     <label htmlFor="input">{input.label}</label>
-                     {createElement(input.type, { key: index, onChange:  })}
+                     {/* <label htmlFor="field">{field.label}</label> */}
+                     {field.component === TextField &&
+                        createElement(field.component, {
+                           key: index,
+                           label: field.label,
+                           placeholder: field.label,
+                           multiline: field.multiline || false,
+                        })}
+                     {field.component === Select && (
+                        <FormControl>
+                           <InputLabel>{field.label}</InputLabel>
+
+                           {createElement(
+                              field.component,
+                              { width: 250 },
+                              field.options.map((option, index) => {
+                                 return (
+                                    <MenuItem key={index} value={option}>
+                                       {option}
+                                    </MenuItem>
+                                 );
+                              })
+                           )}
+                        </FormControl>
+                     )}
                   </div>
                );
             })}
 
-            <button type='submit' onSubmit={}></button>
+            <button type="submit">Save</button>
          </form>
       </div>
    );
--- src/components/resumeEditor/Config.jsx
+++ src/components/resumeEditor/Config.jsx
@@ -1,43 +1,126 @@
+import TextField from '@mui/material/TextField';
+import Select from '@mui/material/Select';
+
 const config = {
    'general Info': [
-      { id: 'fullName', type: 'input', label: 'Full Name' },
-      { id: 'email', type: 'input', label: 'Email' },
-      { id: 'phone', type: 'input', label: 'Phone' },
-      { id: 'address', type: 'textarea', label: 'Address' },
+      {
+         id: 'fullName',
+         component: TextField,
+         type: 'text',
+         label: 'Full Name',
+      },
+      { id: 'email', component: TextField, type: 'email', label: 'Email' },
+      { id: 'phone', component: TextField, type: 'tel', label: 'Phone' },
+      {
+         id: 'address',
+         component: TextField,
+         label: 'Address',
+      },
    ],
    education: [
-      { id: 'degree', type: 'input', label: 'Degree' },
-      { id: 'school', type: 'input', label: 'School' },
-      { id: 'graduationYear', type: 'input', label: 'Graduation Year' },
+      { id: 'degree', component: TextField, type: 'text', label: 'Degree' },
+      { id: 'school', component: TextField, type: 'text', label: 'School' },
+      {
+         id: 'graduationYear',
+         component: TextField,
+         type: 'text',
+         label: 'Graduation Year',
+      },
    ],
    experience: [
-      { id: 'position', type: 'input', label: 'Position' },
-      { id: 'company', type: 'input', label: 'Company' },
-      { id: 'workDescription', type: 'textarea', label: 'Work Description' },
+      { id: 'position', component: TextField, type: 'text', label: 'Position' },
+      { id: 'company', component: TextField, type: 'text', label: 'Company' },
+      {
+         id: 'workDescription',
+         component: TextField,
+         label: 'Work Description',
+         multiline: true,
+      },
    ],
    projects: [
-      { id: 'projectName', type: 'input', label: 'Project Name' },
-      { id: 'description', type: 'textarea', label: 'Project Description' },
-      { id: 'projectLink', type: 'input', label: 'Project Link' },
+      {
+         id: 'projectName',
+         component: TextField,
+         type: 'text',
+         label: 'Project Name',
+      },
+      {
+         id: 'description',
+         component: TextField,
+         label: 'Project Description',
+      },
+      {
+         id: 'projectLink',
+         component: TextField,
+         type: 'text',
+         label: 'Project Link',
+      },
    ],
    skills: [
-      { id: 'skillName', type: 'input', label: 'Skill Name' },
+      {
+         id: 'skillName',
+         component: TextField,
+         type: 'text',
+         label: 'Skill Name',
+      },
       {
          id: 'proficiency',
-         type: 'select',
+         component: Select,
          label: 'Proficiency',
          options: ['Beginner', 'Intermediate', 'Advanced'],
       },
    ],
    languages: [
-      { id: 'language', type: 'input', label: 'Language' },
+      { id: 'language', component: TextField, type: 'text', label: 'Language' },
       {
          id: 'proficiency',
-         type: 'select',
+         component: Select,
          label: 'Proficiency',
          options: ['Elementary', 'Intermediate', 'Fluent'],
       },
    ],
 };
+
+// const config = {
+//    'general Info': [
+//       { id: 'fullName', type: 'input', label: 'Full Name' },
+//       { id: 'email', type: 'input', label: 'Email' },
+//       { id: 'phone', type: 'input', label: 'Phone' },
+//       { id: 'address', type: 'textarea', label: 'Address' },
+//    ],
+//    education: [
+//       { id: 'degree', type: 'input', label: 'Degree' },
+//       { id: 'school', type: 'input', label: 'School' },
+//       { id: 'graduationYear', type: 'input', label: 'Graduation Year' },
+//    ],
+//    experience: [
+//       { id: 'position', type: 'input', label: 'Position' },
+//       { id: 'company', type: 'input', label: 'Company' },
+//       { id: 'workDescription', type: 'textarea', label: 'Work Description' },
+//    ],
+//    projects: [
+//       { id: 'projectName', type: 'input', label: 'Project Name' },
+//       { id: 'description', type: 'textarea', label: 'Project Description' },
+//       { id: 'projectLink', type: 'input', label: 'Project Link' },
+//    ],
+//    skills: [
+//       { id: 'skillName', type: 'input', label: 'Skill Name' },
+//       {
+//          id: 'proficiency',
+//          type: 'select',
+//          label: 'Proficiency',
+//          options: ['Beginner', 'Intermediate', 'Advanced'],
+//       },
+//    ],
+//    languages: [
+//       { id: 'language', type: 'input', label: 'Language' },
+//       {
+//          id: 'proficiency',
+//          type: 'select',
+//          label: 'Proficiency',
+//          options: ['Elementary', 'Intermediate', 'Fluent'],
+//       },
+//    ],
+// };
 
 export default config;
